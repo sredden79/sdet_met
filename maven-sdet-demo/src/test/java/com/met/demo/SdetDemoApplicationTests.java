fe9a380fde05c66e1fabef1bcb6a53f5df9651b3
Scott <scott.redden@show-me-it-ltd.com>
2021-10-27 17:36:48 +0100

    added maven version of the template as more familar with maven

---
+++ maven-sdet-demo/src/test/java/com/met/demo/SdetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.met.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
